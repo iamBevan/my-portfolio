7399d841e5e8093eea6d6ea2c70cec66bf15b634
Kevin <iambevan@gmail.com>
2019-10-02 20:39:58 +0100

    updated landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,14 +12,35 @@ import portfolio from '../images/code-white.svg'
 import linkedin from '../images/linkedin-white.svg'
 import email from '../images/email-white.svg'
 
+import awer from '../images/escape-room.svg';
+import ggc from '../images/ggc-name-logo.svg';
+import dota2 from '../images/dota-2-app.svg';
+import gatsby from '../images/gatsby.svg';
+
+import html from '../images/html5-white.svg';
+import css from '../images/css3-white.svg';
+import js from '../images/javascript-white.svg';
+import react from '../images/react-white.svg';
+import react2 from '../images/react.svg';
+
+import myPic from '../images/title-pic.png';
+
+
 const IndexPage = () => (
 	<OuterLayout>
 		<SEO title="Home" />
 		<div className="home-title">
 			<div>&nbsp;</div>
+			<img src={myPic} className="my-img" />
 			<h1>Kevin Bevan</h1>
-			<h2>Front-end Web Developer</h2>
+			<h2>Junior Front-end Web Developer</h2>
 			<div className="icons">
+				<img className="icon-img" alt="" src={html} />
+				<img className="icon-img" alt="" src={css} />
+				<img className="icon-img" alt="" src={js} />
+				<img className="icon-img" alt="" src={react} />
+			</div>
+			{/* <div className="icons">
 				<section>
 					<Link to="/portfolio">
 						<img alt="" src={portfolio} />
@@ -56,9 +77,55 @@ const IndexPage = () => (
 					</a>
 				</section>
 				
-			</div>
+			</div> */}
 		</div>
 		<div className="home-container">
+			<h2>Projects</h2>
+			<div className="main-projects-container">
+				<div className="project-row">
+					<div className="project">
+						<img src={dota2} alt="Avatar" className="project-image" />
+						<div className="project-overlay">
+							<div className="project-text">
+								Description
+								{/* A current project using the OpenDota API. The app is somewhat of a clone of two popular Dota 2 statistics sites, DotaBuff and OpenDota. You can navigate using the menu to check a player's statistics, past matches, heroes played and current rankings. */}
+							</div>
+						</div>
+					</div>
+					<div className="project">
+						<img src={ggc} alt="Avatar" className="project-image" />
+						<div className="project-overlay">
+							<div className="project-text">Description</div>
+						</div>
+					</div>
+					<div className="project">
+						<img src={awer} alt="Avatar" className="project-image" />
+						<div className="project-overlay">
+							<div className="project-text">Description</div>
+						</div>
+					</div>
+					<div className="project">
+						<img src={gatsby} alt="Avatar" className="project-image" />
+						<div className="project-overlay">
+							<div className="project-text">Description</div>
+						</div>
+					</div>
+				</div>
+				{/* <div className="project-row">
+					<div className="project">
+						<img src={react2} alt="Avatar" clasNames="project-image" />
+						<div className="project-overlay">
+							<div className="project-text">Hello World</div>
+						</div>
+					</div>
+					<div className="project">
+						<img src={react2} alt="Avatar" clasNames="project-image" />
+						<div className="project-overlay">
+							<div className="project-text">Hello World</div>
+						</div>
+					</div>
+				</div> */}
+			</div>
 			<div className="about">
 				<h2>About me</h2>
 				<p>
